aa6d5591012e31a7a1698b9306ad2269f254cc44
Ian Linton <ilinton2571@gmail.com>
2021-11-14 13:29:01 -0700

    added fight or skip option

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,9 @@
-
-
 // player base stats
 var playerName = window.prompt("Name your robot for glorious combat!");
 var playerHealth = 100;
 var playerAttack = 10;
-console.log(playerName, playerHealth, playerAttack);
+var playerMoney = 10;
+console.log(playerName, playerHealth, playerAttack, playerMoney);
 
 // enemy base stats
 var enemyName = "Roborto";
@@ -14,32 +13,59 @@ var enemyAttack = 12;
 // everything inside of the below curly brace is a whole function - REMEMBER THIS!!
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
-    // Subtract playerAttack from enemyHealth
-    enemyHealth = enemyHealth - playerAttack;
-    // console log that
-    // can line break in the parenthesis to make reading this easier
-    console.log(
-        playerName + " attacked " + enemyName + "! " + enemyName + " now has " + enemyHealth + " health remaining!"
-    );
-    // enemy health check
-        if (enemyHealth <= 0) {
-            window.alert("A devastating blow! " + enemyName + " has been defeated!");
-        } 
-        else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left!");
-        }
-    // subtract enemyAttack from playerHealth
-    playerHealth = playerHealth - enemyAttack;
-    // console log that too
-    console.log(
-        enemyName + " attacks " + playerName + "! " + playerName + " now has " + playerHealth + " health remaining!"
-    );
-    // player health check
-        if (playerHealth <= 0) {
-            window.alert("A merciless attack! " + playerName + " has been defeated!");
+    var promptFight = window.prompt("Will you FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        console.log(promptFight);
+    
+        // if player chooses to fight:
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+    
+        // Subtract playerAttack from enemyHealth
+        enemyHealth = enemyHealth - playerAttack;
+        
+        // console log that
+        // can line break in the parenthesis to make reading this easier
+        console.log(
+            playerName + " attacked " + enemyName + "! " + enemyName + " now has " + enemyHealth + " health remaining!"
+        );
+        
+        // enemy health check
+            if (enemyHealth <= 0) {
+                window.alert("A devastating blow! " + enemyName + " has been defeated!");
+            } 
+            else {
+                window.alert(enemyName + " still has " + enemyHealth + " health left!");
+            }
+        
+            // subtract enemyAttack from playerHealth
+        playerHealth = playerHealth - enemyAttack;
+        
+        // console log that too
+        console.log(
+            enemyName + " attacks " + playerName + "! " + playerName + " now has " + playerHealth + " health remaining!"
+        );
+    
+        // player health check
+            if (playerHealth <= 0) {
+                window.alert("A merciless attack! " + playerName + " has been defeated!");
+            } else {
+                window.alert(playerName + " won't take that lying down! " + playerName + " still has " + playerHealth + " health remaining!");
+            }
+        
+        // if player chooses to skip:
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        // quit confirm
+        var confirmSkip = window.confirm("You will be penalized if you skip this fight! Are you sure?")
+        
+        // if yes (true), skip battle
+        if (confirmSkip) {
+            window.alert(playerName + " has chosen to live another day! 2 money lost!");
+            playerMoney = playerMoney - 2;
         } else {
-            window.alert(playerName + " won't take that lying down! " + playerName + " still has " + playerHealth + " health remaining!");
+            fight();
         }
+    } else {
+        window.alert("You must choose a valid option! Try again!");
+    }
 };
 // end of fight function
 
