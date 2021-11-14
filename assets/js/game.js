a18b3976673651a10dcc2578bb4001ed924d3310
Ian Linton <ilinton2571@gmail.com>
2021-11-14 13:01:56 -0700

    added window alerts for fight prompts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,12 @@
 
 
-// player stats
+// player base stats
 var playerName = window.prompt("Name your robot for glorious combat!");
 var playerHealth = 100;
 var playerAttack = 10;
 console.log(playerName, playerHealth, playerAttack);
 
-// enemy stats
+// enemy base stats
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
@@ -21,13 +21,27 @@ var fight = function() {
     console.log(
         playerName + " attacked " + enemyName + "! " + enemyName + " now has " + enemyHealth + " health remaining!"
     );
+    // enemy health check
+        if (enemyHealth <= 0) {
+            window.alert("A devastating blow! " + enemyName + " has been defeated!");
+        } 
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left!");
+        }
     // subtract enemyAttack from playerHealth
     playerHealth = playerHealth - enemyAttack;
     // console log that too
     console.log(
         enemyName + " attacks " + playerName + "! " + playerName + " now has " + playerHealth + " health remaining!"
     );
+    // player health check
+        if (playerHealth <= 0) {
+            window.alert("A merciless attack! " + playerName + " has been defeated!");
+        } else {
+            window.alert(playerName + " won't take that lying down! " + playerName + " still has " + playerHealth + " health remaining!");
+        }
 };
+// end of fight function
 
 // this is calling the function
 fight();
